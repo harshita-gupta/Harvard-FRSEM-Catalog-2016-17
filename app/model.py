ec1ed49d4856ddf0c73514aa67b7473a9bcf52a6
harshita-gupta <gupta.harshita.001@gmail.com>
2016-08-05 17:48:53 -0700

    Edited files to conform to PEP guidelines

--- app/model.py
+++ app/model.py
@@ -1,14 +1,15 @@
 from aenum import Enum
 
+
 class TimeBlock:
     def __init__(self, day, startH, startM, endH, endM):
         self.day = day
-        self.startTime = startH + (startM/60)
-        if self.startTime<9 and self.startTime!= 0:
+        self.startTime = startH + (startM / 60)
+        if self.startTime < 9 and self.startTime != 0:
             self.startTime = self.startTime + 12
 
-        self.endTime = endH + (endM/60)
-        if self.endTime<11.1 and self.endTime!= 0:
+        self.endTime = endH + (endM / 60)
+        if self.endTime < 11.1 and self.endTime != 0:
             self.endTime = self.endTime + 12
 
     def conflicts(self, withTimeBlock):
@@ -20,8 +21,11 @@ class TimeBlock:
             return False
         return True
 
+
 class Seminar:
-    def __init__(self, name, instructor, courseNum, catalogNum, sem, cap, classTimes, timeString, loc, description, website):
+    def __init__(self,
+                 name, instructor, courseNum, catalogNum, sem, cap,
+                 classTimes, timeString, loc, description, website):
         self.name = name
         self.instructor = instructor
         self.courseNum = courseNum
@@ -29,19 +33,22 @@ class Seminar:
         self.capacity = cap
         self.location = loc
         if sem == "Offered Fall Semester":
-            self.fallSem = True;
+            self.fallSem = True
         else:
-            self.fallSem = False;
+            self.fallSem = False
         self.description = description
         self.website = website
         self.timeString = timeString.split("Please note")[0]
-        if self.timeString.endswith(" - "): self.timeString = self.timeString.replace(" - ", "")
+        if self.timeString.endswith(" - "):
+            self.timeString = self.timeString.replace(" - ", "")
         self.timeObj = classTimes
         self.applyLink = ""
 
     def __str__(self):
         esc = "\n"
-        str = repr(self.name) + esc +"Instructor: " + repr(self.instructor) + esc + "Course Number: " + repr(self.courseNum) + esc
+        str = repr(self.name) + esc
+        str += "Instructor: " + repr(self.instructor) + esc
+        str += "Course Number: " + repr(self.courseNum) + esc
         if self.fallSem:
             str += "Fall 2016" + esc
         else:
@@ -51,8 +58,9 @@ class Seminar:
         str += repr(self.location) + esc
         str += repr(self.timeString) + esc
         str += repr(self.description) + esc
-        str += "Course website: " +repr(self.website)
+        str += "Course website: " + repr(self.website)
         return str
+
 
 class Day(Enum):
     Unknown = 0
@@ -62,31 +70,56 @@ class Day(Enum):
     Thursday = 4
     Friday = 5
 
+
 def filterSeminars(seminarList, fallTerm, springTerm, conflicts, searchTerms):
     conflictTimes = []
     searchTerms = searchTerms.split()
-    for conflict in conflicts: conflictTimes.extend(timeStringToTimeBlockObjects(str(" and ".join(conflict.get("days")))  + ", " + str(conflict.get("starttime")) + "-" + str(conflict.get("endtime"))))
+    for conflict in conflicts:
+        conflictTimes.extend(
+            timeStringToTimeBlockObjects(
+                str(" and ".join(conflict.get("days"))) +
+                ", " +
+                str(conflict.get("starttime")) +
+                "-" +
+                str(conflict.get("endtime"))))
 
-    seminars = [seminar for seminar in seminarList if ((((seminar.fallSem == True and fallTerm == True) or (seminar.fallSem == False and springTerm == True))) and (not any(seminarTime.conflicts(conflictTime) for conflictTime in conflictTimes for seminarTime in seminar.timeObj)))]
-    if searchTerms: seminars = [x for x in seminars if any(keyword.lower() in str(x).lower() for keyword in searchTerms)]
+    seminars = [seminar for seminar in seminarList if
+                ((((seminar.fallSem is True and fallTerm is True) or
+                    (seminar.fallSem is False and springTerm is True))) and
+                    (not any(seminarTime.conflicts(conflictTime)
+                             for conflictTime in conflictTimes
+                             for seminarTime in seminar.timeObj)))]
+    if searchTerms:
+        seminars = [x for x in seminars if
+                    any(keyword.lower() in str(x).lower()
+                        for keyword in searchTerms)]
 
     return seminars
 
 
 def daysForString(str):
-    days = str.replace(" & "," and ").split(" and ")
+    days = str.replace(" & ", " and ").split(" and ")
     daysToReturn = []
     for day in days:
-        daysToReturn.append({"m": Day.Monday,"monday": Day.Monday,"mon": Day.Monday,"tues": Day.Tuesday,"t": Day.Tuesday,"tuesday": Day.Tuesday,"wednesday": Day.Wednesday,"w": Day.Wednesday,"wed": Day.Wednesday,"thursday": Day.Thursday,"th": Day.Thursday,"thurs": Day.Thursday,"friday": Day.Friday,"fri": Day.Friday,"f": Day.Friday}.get(day.lower(), Day.Unknown))
+        daysToReturn.append(
+            {"m": Day.Monday, "monday": Day.Monday, "mon": Day.Monday,
+             "tues": Day.Tuesday, "t": Day.Tuesday, "tuesday": Day.Tuesday,
+             "wednesday": Day.Wednesday, "w": Day.Wednesday,
+             "wed": Day.Wednesday,
+             "thursday": Day.Thursday, "th": Day.Thursday,
+             "thurs": Day.Thursday,
+             "friday": Day.Friday, "fri": Day.Friday, "f": Day.Friday}.
+            get(day.lower(), Day.Unknown))
     return daysToReturn
+
 
 def timeStringToTimeBlockObjects(str):
     if "[unavailable]" in str:
-        return [TimeBlock(Day.Unknown, 0,0,0, 0)]
+        return [TimeBlock(Day.Unknown, 0, 0, 0, 0)]
 
     timeStrings = str.split(",")
-    days = daysForString(timeStrings[0].replace(",",""))
-    timeblocks=[]
+    days = daysForString(timeStrings[0].replace(",", ""))
+    timeblocks = []
     for day in days:
         if day != Day.Unknown:
             times = timeStrings[1].strip().split(" ")[0].split("-")
@@ -99,7 +132,8 @@ def timeStringToTimeBlockObjects(str):
             else:
                 startM = float(0)
 
-            endH = float(endTimes[0].replace("pm", "").replace("am", "").replace("N", ""))
+            endH = float(endTimes[0].replace("pm", "").
+                         replace("am", "").replace("N", ""))
             if len(endTimes) > 1:
                 endM = float(endTimes[1])
             else:
--- app/views.py
+++ app/views.py
@@ -1,21 +1,13 @@
-from copy import deepcopy
-from flask import render_template,  request, jsonify
+from flask import render_template, request
 from app import app
 import pickle
-from StringIO import StringIO
-import pycurl
-import re
-from bs4 import BeautifulSoup
-from aenum import Enum
 import socket
 import keen
-from model import Seminar, Day, filterSeminars, TimeBlock
-from datasource import linkToSeminarObj, retrieveSeminars
+from model import filterSeminars
+from datasource import retrieveSeminars
 
 keen.project_id = "57a4ec410727190b418cc7fc"
 keen.write_key = "3e8d74ce621627a69bb65ba5a0e7eb4877fe51c1ea62703708a3c28e5bbaaf53c28f0b97d7f3baddf19d98e9a08b634c88fbc13c9c3b39a46873f2f0154dd4e461488c13bce00d4cb2c913a2586c3fff6151d3540b74d6b3518cc3eba8b7cbcf"
-KEEN_PROJECT_ID="57a4ec410727190b418cc7fc"
-KEEN_WRITE_KEY="3e8d74ce621627a69bb65ba5a0e7eb4877fe51c1ea62703708a3c28e5bbaaf53c28f0b97d7f3baddf19d98e9a08b634c88fbc13c9c3b39a46873f2f0154dd4e461488c13bce00d4cb2c913a2586c3fff6151d3540b74d6b3518cc3eba8b7cbcf"
 
 alert = ""
 applyLinks = ""
@@ -35,20 +27,34 @@ def home():
         searchKeywords = str(form['searchquery'])
         fallTerm = True if form.get('fallterm') else False
         springTerm = True if form.get('springterm') else False
-        allConflictValues = {k: v for k,v in form.iteritems() if k.startswith("conflict")}
+        allConflictValues = {k: v for k, v in form.iteritems()
+                             if k.startswith("conflict")}
         conflicts = []
-        numNextConflict = 1 
+        numNextConflict = 1
         moreConflicts = True
         while moreConflicts:
-            if allConflictValues.get("conflict"+ str(numNextConflict) + "-day"):
-                conflicts.append({"days": form.getlist("conflict"+ str(numNextConflict) + "-day"), "starttime" :  allConflictValues.get("conflict"+ str(numNextConflict) + "-starttime"), "endtime" : allConflictValues.get("conflict"+ str(numNextConflict) + "-endtime")})
-                numNextConflict+= 1
+            if allConflictValues.get("conflict" +
+                                     str(numNextConflict) + "-day"):
+                conflicts.append(
+                    {"days": form.getlist(
+                        "conflict" + str(numNextConflict) + "-day"),
+                     "starttime": allConflictValues.get(
+                        "conflict" + str(numNextConflict) + "-starttime"),
+                     "endtime": allConflictValues.get(
+                        "conflict" + str(numNextConflict) + "-endtime")})
+                numNextConflict += 1
             else:
                 moreConflicts = False
-        keen.add_event("seminar_loads", {"fallterm" : str(fallTerm), "springterm" : str(springTerm), "searches": str(searchKeywords), "conflicts": str(conflicts) })
-        seminarsToDisplay = filterSeminars(seminars, fallTerm, springTerm, conflicts, searchKeywords)
+        keen.add_event("seminar_loads",
+        {"fallterm" : str(fallTerm),
+        "springterm" : str(springTerm),
+        "searches": str(searchKeywords),
+        "conflicts": str(conflicts) })
+        seminarsToDisplay = filterSeminars(
+            seminars, fallTerm, springTerm, conflicts, searchKeywords)
 
-        return render_template('home.html', seminars= seminarsToDisplay, alert = alert)
+        return render_template(
+            'home.html', seminars=seminarsToDisplay, alert=alert)
 
-    keen.add_event("homepage_loads", {"ip":request.remote_addr})
+    # keen.add_event("homepage_loads", {"ip":request.remote_addr})
     return render_template('home.html')
